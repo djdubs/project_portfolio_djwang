e92193942c056f045df5207f94860bc8abc3333f
djdubs <daniel.jdubs@gmail.com>
2026-07-28 11:38:14 -0400

    7/28/2026

---
+++ projects/rscripts/out_file.r
@@ -0,0 +1,206 @@
+# Daniel Wang
+# DSA 595 - Semester Project
+# 4/22/25
+
+setwd("G:/My Drive/Spring 2025/DSA 595/project")
+source("run_file.r")
+
+
+# Dataset subsetted for pitchers in 2024 and batting average has been converted to a percentage  
+pitching_2015_2024 <- read.csv("stats_pitching.csv", header = T)
+pitch <- pitching_2015_2024[pitching_2015_2024$year==2024,]
+pitch$batting_avg.per <- pitch$batting_avg*100
+
+# _____________________________________________________________________________
+## Exponential Model
+# Implementing Bayesian computational algorithm on the dataset
+set.seed(5)
+X = cbind(1, pitch$batting_avg.per, pitch$exit_velocity_avg)
+n = nrow(pitch)
+p = ncol(X)
+
+y = pitch$p_era
+
+par_index = list( beta=1:p)
+
+init_par = c( rep( 0, p))
+prior_par = list( prior_mean=rep(0, p), prior_sd=c(1.23, 0.01, 0.01))
+steps = 20000
+burnin = 5000
+
+metro.exp.out <- metro(y, X, log_density_exp, init_par, prior_par, par_index, steps, burnin)
+
+# _____________________________________________________________________________
+# Estimating the parameters
+chain.exp.out <- metro.exp.out$chain
+beta.hat.a <- colMeans(chain.exp.out)
+var1.hat.a <- var(chain.exp.out[,2])
+var2.hat.a <- var(chain.exp.out[,3])
+
+
+# _____________________________________________________________________________
+# Simulation Study with synthetic data
+seeds <- seq(46,55)
+metro.exp.sim <- list()
+
+p = 3
+beta <- beta.hat.a
+par_true.a = c( beta)
+
+n = nrow(pitch)
+
+par_index = list( beta=1:p)
+
+init_par = c( rep( 0, p))
+prior_par = list( prior_mean=rep(0, p), prior_sd=c(1.23, 0.01, 0.01))
+steps = 20000
+burnin = 5000
+
+for (i in seeds) {
+  set.seed(i)
+  X.syn <- cbind(1, rnorm(n, mean = 25, sd = 4), rnorm(n, mean = 88, sd = 1.5))
+  
+  y.syn <- rgamma(n, shape = 1, scale = exp(X.syn %*% beta))
+  
+  metro.exp.sim[[i-45]] <- metro(y.syn, X.syn, log_density_exp, init_par, prior_par, par_index, steps, burnin)
+}
+
+exp.chains <- lapply(metro.exp.sim, function(x) x$chain)
+
+exp.est <- lapply(exp.chains, colMeans)
+exp.sim.totals <- do.call(rbind, exp.chains)
+
+
+# _____________________________________________________________________________
+# MCMC trace plots and histograms
+n_post = 15000
+index_post = (steps - burnin - n_post + 1):(steps - burnin)
+chain.exp.sim = metro.exp.sim[[1]]$chain[index_post,]
+param.names <- c("Intercept", "Batting Avg.", "EV Avg.", "V")
+
+
+par_mean = par_median = upper = lower = rep( NA, ncol(chain.exp.sim))
+pdf("tplots_expmod.pdf")
+par(mfrow=c(3, 2))
+for(r in 1:ncol(chain.exp.sim)){
+  
+  plot( NULL, xlab=NA, ylab=NA, xlim=c(1,length(index_post)), 
+        main=paste(param.names[r]), ylim=range(chain.exp.sim[,r]) )
+  
+  lines( chain.exp.sim[,r], type='p', col='black')
+  
+  par_mean[r] = round( mean(chain.exp.sim[,r]), 4)
+  par_median[r] = round( median(chain.exp.sim[,r]), 4)
+  upper[r] = round( quantile( chain.exp.sim[,r], prob=.975), 4)
+  lower[r] = round( quantile( chain.exp.sim[,r], prob=.025), 4)
+  
+  hist( chain.exp.sim[,r], ylab=NA, main=NA, freq=F,
+        breaks=sqrt(nrow(chain.exp.sim)),
+        xlab=paste0('Mean = ',toString(par_mean[r]),
+                    ' Median = ',toString(par_median[r])))
+  abline( v=upper[r], col='red', lwd=2, lty=2)
+  abline( v=lower[r], col='purple', lwd=2, lty=2)
+  
+  abline( v=par_true.a[r], col='green', lwd=2, lty=2)
+}
+dev.off()
+
+# _____________________________________________________________________________
+## Gamma Model
+# Implementing Bayesian computational algorithm on the dataset
+set.seed(20)
+X = cbind(1, pitch$batting_avg.per, pitch$exit_velocity_avg)
+n = nrow(pitch)
+p = ncol(X)
+
+y = pitch$p_era
+
+par_index = list( beta=1:p, v=p+1)
+
+init_par = c( rep( 0, p), exp(0))
+prior_par = list( prior_mean=rep(0, p), prior_sd=c(1.23, 0.01, 0.01),
+                  v_shape=2, v_scale=2)
+steps = 20000
+burnin = 5000
+
+metro.gam.out <- metro(y, X, log_density_gamma, init_par, prior_par, par_index, steps, burnin)
+
+
+
+# _____________________________________________________________________________
+# Estimating the parameters
+chain.gamma.out <- metro.gam.out$chain
+beta.hat.b <- colMeans(chain.gamma.out)
+var1.hat.b <- var(chain.gamma.out[,2])
+var2.hat.b <- var(chain.gamma.out[,3])
+var3.hat.b <- var(chain.gamma.out[,4])
+
+
+# _____________________________________________________________________________
+# Simulation Study with synthetic data
+seeds <- seq(31, 40)
+metro.gam.sim <- list()
+
+p = 3
+beta <- beta.hat.b[1:p]
+v <- beta.hat.b[p+1]
+par_true.b = c(beta, v)
+
+n = nrow(pitch)
+
+par_index = list( beta=1:p, v=p+1)
+
+init_par = c( rep( 0, p), exp(0))
+prior_par = list( prior_mean=rep(0, p), prior_sd=c(1.23, 0.01, 0.01),
+                  v_shape=2, v_scale=2)
+steps = 20000
+burnin = 5000
+
+for (i in seeds) {
+  set.seed(i)
+  
+  X.syn <- cbind(1, rnorm(n, mean = 25, sd = 4), rnorm(n, mean = 88, sd = 1.5))
+  
+  y.syn <- rgamma(n, shape = v, scale = exp(X.syn %*% beta)/v)
+  
+  metro.gam.sim[[i-30]] <- metro(y.syn, X.syn, log_density_gamma, init_par, prior_par, par_index, steps, burnin)
+}
+
+gam.chains <- lapply(metro.gam.sim, function(x) x$chain)
+
+gam.est <- lapply(gam.chains, colMeans)
+gam.sim.totals <- do.call(rbind, gam.chains)
+
+# _____________________________________________________________________________
+# MCMC trace plots and histograms
+n_post = 15000
+index_post = (steps - burnin - n_post + 1):(steps - burnin)
+chain.gam.sim = metro.gam.sim[[1]]$chain[index_post,]
+param.names <- c("Intercept", "Batting Avg.", "EV Avg.", "Shape V")
+
+
+par_mean = par_median = upper = lower = rep( NA, ncol(chain.gam.sim))
+pdf("tplots_gammod.pdf")
+par(mfrow=c(2, 2))
+for(r in 1:ncol(chain.gam.sim)){
+  
+  plot( NULL, xlab=NA, ylab=NA, xlim=c(1,length(index_post)), 
+        main=paste(param.names[r]), ylim=range(chain.gam.sim[,r]) )
+  
+  lines( chain.gam.sim[,r], type='p', col='black')
+  
+  par_mean[r] = round( mean(chain.gam.sim[,r]), 4)
+  par_median[r] = round( median(chain.gam.sim[,r]), 4)
+  upper[r] = round( quantile( chain.gam.sim[,r], prob=.975), 4)
+  lower[r] = round( quantile( chain.gam.sim[,r], prob=.025), 4)
+  
+  hist( chain.gam.sim[,r], ylab=NA, main=NA, freq=F,
+        breaks=sqrt(nrow(chain.gam.sim)),
+        xlab=paste0('Mean = ',toString(par_mean[r]),
+                    ' Median = ',toString(par_median[r])))
+  abline( v=upper[r], col='red', lwd=2, lty=2)
+  abline( v=lower[r], col='purple', lwd=2, lty=2)
+  
+  abline( v=par_true.b[r], col='green', lwd=2, lty=2)
+}
+dev.off()
---
+++ projects/rscripts/run_file.R
@@ -0,0 +1,183 @@
+# Daniel Wang
+# DSA 595 - Semester Project
+# 4/22/25
+
+library(mvtnorm)
+library(invgamma)
+
+# _____________________________________________________________________________
+# PDF of the gamma distribution with new parameterization
+
+log.gamma.pdf <- function(y, v, mu) {
+  
+  expr <- (1/gamma(v)) * (v/mu)^v * y^(v-1) * exp(-v*y/mu)
+  
+  return(log(expr))
+}
+# _____________________________________________________________________________
+
+
+
+
+# _____________________________________________________________________________
+# Function to compute log-likelihood of the data (Y~exponential)
+
+log_density_exp <- function( par, prior_par, par_index, y, X) {
+  
+  n = length(y)
+  beta = matrix(par[par_index$beta], ncol=1)
+  
+  log_fn <- sum(dgamma(c(y), shape = 1, scale = exp(X%*%beta), log = T))
+  
+  beta.mean = prior_par$prior_mean[par_index$beta]
+  beta.sd = diag(prior_par$prior_sd[par_index$beta])
+  
+  log_pd_beta = dmvnorm(x=c(beta), mean=beta.mean, sigma=beta.sd, log=T)
+  
+  return(log_fn + log_pd_beta)
+}
+# _____________________________________________________________________________
+
+
+
+
+# _____________________________________________________________________________
+# Function to compute log-likelihood of the data (Y~gamma)
+
+log_density_gamma <- function( par, prior_par, par_index, y, X) {
+  
+  n = length(y)
+  beta = matrix(par[par_index$beta], ncol=1)
+  v = par[par_index$v]
+  
+  log_fn <- sum(log.gamma.pdf(c(y), v, exp(X %*% beta)))
+  
+  beta.mean = prior_par$prior_mean[par_index$beta]
+  beta.sd = diag(prior_par$prior_sd[par_index$beta])
+  v_shape = prior_par$v_shape
+  v_scale = prior_par$v_scale
+  
+  log_pd_beta = dmvnorm(x=c(beta), mean=beta.mean, sigma=beta.sd, log=T)
+  
+  log_pd_v = dgamma(x=v, shape = v_shape, scale = v_scale, log = T)
+  
+  return(log_fn + log_pd_beta + log_pd_v)
+}
+# _____________________________________________________________________________
+
+
+
+
+# _____________________________________________________________________________
+## MCMC routine using MH algorithm
+metro <- function(y, X, log_d, init_par, prior_par, par_index, steps, burnin) {
+  
+  par = init_par
+  n=length(y)
+  n_par = length(par)
+  chain = matrix( 0, steps, n_par)
+  
+  # referencing different parameters depending on which distribution the response is fitted under
+  group <- par_index
+  n_group = length(group)
+  
+  pcov = list();	for(j in 1:n_group)  pcov[[j]] = diag(length(group[[j]]))
+  pscale = rep(.0001, n_group)
+  accept = rep(0, n_group)
+  
+  # Evaluate the log_post of the initial par
+  log_density <- log_d
+  log_post_prev = log_density(par, prior_par, par_index, y, X)
+  if(!is.finite(log_post_prev)){
+    print("Infinite log-posterior; choose better initial parameters")
+    break
+  }
+  
+  # ___________________________________________________________________________
+  
+  # Begin Algorithm
+  chain[1,] = par
+  for(i in 2:steps) {
+    for (j in 1:n_group) {
+      
+      # Propose an update
+      ind_j = group[[j]]
+      proposal = par
+      if (j==1) {
+        proposal[ind_j]=rmvnorm(n=1, mean=par[ind_j], sigma=pcov[[j]]*pscale[j])
+      } else {
+        proposal[ind_j]=rgamma(n=1, shape=par[ind_j]/exp(pcov[[j]]*pscale[j]), scale=exp(pcov[[j]]*pscale[j]))
+      }
+      
+      # Compute the log density for the proposal
+      log_post = log_density(proposal, prior_par, par_index, y, X)
+      
+      # Only propose valid parameters during the burnin period
+      if(i < burnin){
+        while(!is.finite(log_post)){
+          print('bad proposal')
+          proposal = par
+          if (j==1) {
+            proposal[ind_j]=rmvnorm(n=1, mean=par[ind_j], sigma=pcov[[j]]*pscale[j])
+          } else {
+            proposal[ind_j]=rgamma(n=1, shape=par[ind_j]/exp(pcov[[j]]*pscale[j]), scale=exp(pcov[[j]]*pscale[j]))
+          }
+          log_post = log_density( proposal, prior_par, par_index, y, X)
+        }		
+      }
+      
+      # Evaluate the Metropolis-Hastings ratio
+      if( log_post - log_post_prev > log(runif(1,0,1)) ){
+        log_post_prev = log_post
+        par[ind_j] = proposal[ind_j]
+        accept[j] = accept[j] +1
+      }
+      chain[i,ind_j] = par[ind_j]
+      
+      # Proposal tuning scheme ------------------------------------------------
+      if(i < burnin){
+        # During the burnin period, update the proposal covariance in each step 
+        # to capture the relationships within the parameters vectors for each 
+        # transition.  This helps with mixing.
+        if(i == 100)  pscale[j] = 1
+        
+        if(length(ind_j) > 1){
+          if(100 <= i & i <= 2000){  
+            temp_chain = chain[1:i,ind_j]
+            pcov[[j]] = cov(temp_chain[ !duplicated(temp_chain),, drop=F])
+            
+          } else if(2000 < i){  
+            temp_chain = chain[(i-2000):i,ind_j]
+            pcov[[j]] = cov(temp_chain[ !duplicated(temp_chain),, drop=F])
+          }
+          if( sum( is.na(pcov[[j]]) ) > 0)  pcov[[j]] = diag( length(ind_j) )
+        }
+        
+        # Tune the proposal covariance for each transition to achieve 
+        # reasonable acceptance ratios.
+        if(i %% 30 == 0){ 
+          if(i %% 480 == 0){  
+            accept[j] = 0  
+            
+          } else if( accept[j] / (i %% 480) < .4 ){ 
+            pscale[j] = (.75^2)*pscale[j] 
+            
+          } else if( accept[j] / (i %% 480) > .5 ){ 
+            pscale[j] = (1.25^2)*pscale[j]
+          } 
+        }
+      }
+    }
+    # Restart the acceptance ratio at burnin.
+    if(i == burnin)  accept[j] = 0
+    
+    if(i%%100==0)  cat('--->',i,'\n')
+  }
+  
+  print(accept/(steps-burnin))
+  cat("pscale = ",pscale,"\n")
+  cat("pcov = ","\n")
+  print(pcov)
+  return(list( chain=chain[burnin:steps,], accept=accept/(steps-burnin),
+               pscale=pscale))
+}
